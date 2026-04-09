33d97c8dccc27656553ff2fdf76e71f9fafda70d
rohitdixit52 <dixitrohit665@gmail.com>
2026-04-09 15:07:44 +0530

    Added user recommendation API for personalized activity suggestions

---
+++ src/main/java/com/project/fitness/service/RecommendationService.java
@@ -0,0 +1,39 @@
+package com.project.fitness.service;
+
+import com.project.fitness.dto.RecommendationRequest;
+import com.project.fitness.model.Activity;
+import com.project.fitness.model.Recommendation;
+import com.project.fitness.model.User;
+import com.project.fitness.repository.ActivityRepository;
+import com.project.fitness.repository.RecommendationRepository;
+import com.project.fitness.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+
+    private final UserRepository userRepository;
+    private final ActivityRepository activityRepository;
+    private final RecommendationRepository recommendationRepository;
+
+
+    public Recommendation generateRecommendation(RecommendationRequest request) {
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new RuntimeException("User Not Found" + request.getUserId()));
+
+        Activity activity = activityRepository.findById(request.getActivityId())
+                .orElseThrow(() -> new RuntimeException("Activity Not Found" + request.getActivityId()));
+
+        Recommendation recommendation = Recommendation.builder()
+                .user(user)
+                .activity(activity)
+                .improvements(request.getImprovements())
+                .suggestions(request.getSuggestions())
+                .safety(request.getSafety())
+                .build();
+
+        return recommendationRepository.save(recommendation);
+    }
+}
